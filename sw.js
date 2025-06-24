9f2b50c56a3d51cb3a5e8d6eac2fa25dc439ce4c
lwxx <2281846183@qq.com>
2025-06-25 03:41:02 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'moneyer-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
